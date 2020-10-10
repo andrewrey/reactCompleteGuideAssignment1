86d610060d108ee985bb65876ed0c9e253983a25
Andrew Reynolds <andrew_reynolds@mac.com>
2020-10-09 22:33:24 -0700

    started maincontent component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.scss";
 import Header from "./Header";
+import MainContent from "./MainContent";
 
 class App extends Component {
   render() {
     return (
       <div className={"App"}>
         <Header />
+        <MainContent />
       </div>
     );
   }
---
+++ src/MainContent/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./MainContent.scss";
+
+const MainContent = () => (
+  <main>
+    <h2>Guest List</h2>
+  </main>
+);
+export default MainContent;
