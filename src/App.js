08f6ccf66a2aee94501f2714e9b29be800ed117d
Andrew Reynolds <andrew_reynolds@mac.com>
2020-10-10 22:19:43 -0700

    created onchange for input and submit new name for guest state

--- src/App.js
+++ src/App.js
@@ -9,10 +9,25 @@ class App extends Component {
     guests: [],
   };
 
+  lastId = 0;
+
+  updateIDNumber = () => {
+    let id = this.lastId;
+    this.lastId += 1;
+    return id;
+  };
+
   handleNameInput = (e) => {
     console.log(e.target.value);
     this.setState({
-      guests: [{ name: e.target.value }],
+      pendingGuest: e.target.value,
+    });
+  };
+
+  handleNameSubmit = (name) => {
+    let id = this.updateIDNumber();
+    this.setState({
+      guests: [{ name, id }, ...this.state.guests],
     });
   };
 
