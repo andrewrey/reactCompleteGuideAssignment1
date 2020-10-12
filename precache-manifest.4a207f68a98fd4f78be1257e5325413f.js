c5380b69beb7bbd4b8ec51a47043578e04d2fbfd
Andrew Reynolds <andrew_reynolds@mac.com>
2020-10-12 09:53:39 -0700

    Updates

---
+++ precache-manifest.4a207f68a98fd4f78be1257e5325413f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2acabcf49a2b381caf3112c44820dca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd18b2d1cd912f16a882",
+    "url": "/static/css/main.e435059b.chunk.css"
+  },
+  {
+    "revision": "92b607fb5789fbecd49d",
+    "url": "/static/js/2.917d4896.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.917d4896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd18b2d1cd912f16a882",
+    "url": "/static/js/main.1e988457.chunk.js"
+  },
+  {
+    "revision": "72eaaa89a7643c06c39b",
+    "url": "/static/js/runtime-main.74b62403.js"
+  }
+]);
