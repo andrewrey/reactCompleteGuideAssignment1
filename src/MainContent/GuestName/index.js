1cce6ba6a7a93d5bd22828a6110f400614f38d88
Andrew Reynolds <andrew_reynolds@mac.com>
2020-10-11 22:05:49 -0700

    created guest name compoent for guest cards

---
+++ src/MainContent/GuestName/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GuestName = () => {
+  return (
+    <div className="guestCard">
+      <h3>Name</h3>
+    </div>
+  );
+};
+
+export default GuestName;
--- src/MainContent/Output/index.js
+++ src/MainContent/Output/index.js
@@ -1,7 +1,12 @@
 import React from "react";
+import GuestName from "../GuestName";
 
 const Output = () => {
-  return <div className="guest-list"></div>;
+  return (
+    <div className="guest-list">
+      <GuestName />
+    </div>
+  );
 };
 
 export default Output;
