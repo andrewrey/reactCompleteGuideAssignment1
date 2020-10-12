a8f876537c6dad894879da2ebececc35562aa219
Andrew Reynolds <andrew_reynolds@mac.com>
2020-10-12 10:00:15 -0700

    Updates

---
+++ precache-manifest.241cf72e95aef40b33ebfbd0a09b2056.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "407d2d18ebf39190d0f5786dfbf7430d",
+    "url": "/reactCompleteGuideAssignment1/index.html"
+  },
+  {
+    "revision": "89efc50575709f10fe2f",
+    "url": "/reactCompleteGuideAssignment1/static/css/main.e435059b.chunk.css"
+  },
+  {
+    "revision": "92b607fb5789fbecd49d",
+    "url": "/reactCompleteGuideAssignment1/static/js/2.917d4896.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactCompleteGuideAssignment1/static/js/2.917d4896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89efc50575709f10fe2f",
+    "url": "/reactCompleteGuideAssignment1/static/js/main.520ea380.chunk.js"
+  },
+  {
+    "revision": "03dc9f7c607a7a440436",
+    "url": "/reactCompleteGuideAssignment1/static/js/runtime-main.1e066549.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.24a02d22b09dc45beca925b09fca5b83.js"
+  "/reactCompleteGuideAssignment1/precache-manifest.241cf72e95aef40b33ebfbd0a09b2056.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactCompleteGuideAssignment1/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
