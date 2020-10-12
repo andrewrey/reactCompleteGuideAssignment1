ac631142859521f9d4bde2b9fe0601a2b01bacc6
Andrew Reynolds <andrew_reynolds@mac.com>
2020-10-11 21:56:56 -0700

    created guestlist output

---
+++ src/MainContent/Output/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Output = () => {
+  return <div className="guest-list"></div>;
+};
+
+export default Output;
--- src/MainContent/index.js
+++ src/MainContent/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./MainContent.scss";
 import GuestInput from "./GuestInput";
+import Output from "./Output";
 
 const MainContent = ({ handleName, handleSubmit, pendingGuest }) => (
   <main>
     <h2>Guest List</h2>
     <GuestInput handleName={handleName} handleSubmit={handleSubmit} pendingGuest={pendingGuest} />
+    <Output />
   </main>
 );
 export default MainContent;
